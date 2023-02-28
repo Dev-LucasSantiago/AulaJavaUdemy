697600fc85f75a4b33e83f303c23a5ceeb9174d9
lucas <lucas_souza_s@hotmail.com>
2023-02-28 13:03:21 -0300

    Update Aula de Operador Ternario

---
+++ src/Operadores/Ternario.java
@@ -0,0 +1,20 @@
+package Operadores;
+
+public class Ternario {
+    public static void main(String[] args) {
+        double media = 7.6;
+
+        String resultadoParcial = media >= 5.0 ? "Recuperação" : "Reprovado";
+        String resultadoFinal = media >= 7.0 ? "Aprovado" : resultadoParcial;
+        System.out.println("O aluno está: "+resultadoFinal);
+
+        double nota = 9.9;
+        boolean bomComportamento = true;
+        boolean passouPorMedia = nota >= 7;
+        boolean temDesconto = bomComportamento && passouPorMedia;
+
+        String resultado = temDesconto ? "Sim." : "Não.";
+        System.out.printf("Tem desconto? %s", resultado);
+
+    }
+}
