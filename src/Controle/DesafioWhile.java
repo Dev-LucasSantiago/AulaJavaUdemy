4500d69d76aed7e96e9b49bf23bca6531f858762
lucas <lucas_souza_s@hotmail.com>
2023-03-09 13:13:38 -0300

    testando do DesafioWhile

--- src/Controle/DesafioWhile.java
+++ src/Controle/DesafioWhile.java
@@ -1,7 +1,23 @@
 package Controle;
 
+import java.util.Scanner;
+
 public class DesafioWhile {
     public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
 
+        double nota = 0;
+        double media = 0;
+
+        while (!(nota == -1)){
+            if (nota >= 0 && nota <= 10){
+                System.out.print("Digite a sua nota: ");
+                nota = leitor.nextDouble();
+
+            }
+            media = nota++;
+            System.out.println(media);
+        }
+        System.out.println(media);
     }
 }
