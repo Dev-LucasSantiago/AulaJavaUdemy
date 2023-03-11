5250dd4b8468013a3a9cd2aa621a913ff7c3ceb9
Lucas Santiago <Lucas_souza_s@hotmail.com>
2023-03-11 20:45:29 -0300

    Aula For 3

---
+++ src/Controle/For2.java
@@ -0,0 +1,13 @@
+package Controle;
+
+
+
+public class For2 {
+    public static void main(String[] args) {
+
+        //For Decrementando de 2 em 2 até o 0
+        for (int num=10;num >= 0; num-=2){
+            System.out.println("Numero = "+num);
+        }
+    }
+}
