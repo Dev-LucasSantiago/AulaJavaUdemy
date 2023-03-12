946452f326246b9960311d3d3bf11c519b3d3ebe
Lucas Santiago <Lucas_souza_s@hotmail.com>
2023-03-11 22:23:49 -0300

    Aula de Continue

--- src/Controle/Break.java
+++ src/Controle/Break.java
@@ -2,6 +2,7 @@ package Controle;
 
 public class Break {
     public static void main(String[] args) {
+        //Break ele interrompe e pula para o final
         for (int i = 0; i < 10 ; i++){
             if (i == 5){
                 break;
