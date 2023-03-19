c2b8281542f30e20dd851f29ac0e6d1cd97449b6
Lucas Santiago <Lucas_souza_s@hotmail.com>
2023-03-19 07:24:31 -0300

    Aula de Orientação a Objetos - Update

--- src/Classe/ProdutoTeste.java
+++ src/Classe/ProdutoTeste.java
@@ -2,17 +2,17 @@ package Classe;
 
 public class ProdutoTeste {
     public static void main(String[] args) {
-        Produto p1 = new Produto("Notebook",1600,0.5);
+        Produto p1 = new Produto("Notebook",4500);
 
-        var p2 = new Produto("Caneta Preta",2.50,0.1);
+        var p2 = new Produto("Caneta Preta",12.50);
 
-        System.out.println(p1.nome);
-        System.out.println(p2.nome);
+        System.out.println(p1.nome+" - "+p1.precoComDesconto());
+        System.out.println(p2.nome+" - "+p2.precoComDesconto());
 
         double precoFinal1 = p1.precoComDesconto();
         double precoFinal2 = p2.precoComDesconto(0.1);
         double media = (precoFinal1 + precoFinal2) /2;
 
-        System.out.printf("Valor médio  do carrinho deu: R$%.2f",media);
+        System.out.printf("Valor médio  do carrinho deu: R$%.2f\nCom deconto de %.2f",media,Produto.desconto);
     }
 }
