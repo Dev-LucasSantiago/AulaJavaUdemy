d643412929e6f4fa7221c50d055b71c63772b04e
Lucas Santiago <Lucas_souza_s@hotmail.com>
2023-03-25 10:37:22 -0300

    update

--- src/Classe/Desafio/Jantar.java
+++ src/Classe/Desafio/Jantar.java
@@ -5,8 +5,9 @@ public class Jantar {
         Pessoa p = new Pessoa("Lucas",78);
         Comida c = new Comida("Arroz",0.100);
         Comida b = new Comida("Feijão",0.200);
-        System.out.println("Nome: "+p.nome+"\nPesa: "+p.peso+" Kg\n-------------------------------------");
 
+
+        System.out.println("Nome: "+p.nome+"\nPesa: "+p.peso+" Kg\n-------------------------------------");
         p.comer(c);
         System.out.println("Nome: "+p.nome+"\nPesa: "+p.peso+" Kg\n-------------------------------------");
         p.comer(b);
